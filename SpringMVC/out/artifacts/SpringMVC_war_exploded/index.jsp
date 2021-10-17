17dc395e8ab039472edfd2dce30188ba384dae03
Shuaih7 <shuaih7@gmail.com>
2021-10-18 00:38:57 +0800

    feat: SpringMVC annotation
    note: due to some unknown reasons, the SpringMVC cannot map the annotated servlet.

--- SpringMVC/out/artifacts/SpringMVC_war_exploded/index.jsp
+++ SpringMVC/out/artifacts/SpringMVC_war_exploded/index.jsp
@@ -1,8 +1,8 @@
 <%--
   Created by IntelliJ IDEA.
   User: shuai
-  Date: 10/14/2021
-  Time: 11:15 PM
+  Date: 10/17/2021
+  Time: 5:57 PM
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
--- SpringMVC/src/main/java/com/shuai/controller/HelloController.java
+++ SpringMVC/src/main/java/com/shuai/controller/HelloController.java
@@ -1,27 +1,20 @@
 package com.shuai.controller;
 
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
+@ResponseBody
+@RequestMapping("/hello")
+public class HelloController {
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+    @RequestMapping("/test")
+    public String sayHello(Model model){
+        System.out.println("Connected to the controller.");
+        model.addAttribute("msg", "Hello, SpringMVC.");
 
-
-//注意：这里我们先导入Controller接口
-public class HelloController implements Controller {
-
-
-    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        //ModelAndView 模型和视图
-        ModelAndView mv = new ModelAndView();
-
-
-        //封装对象，放在ModelAndView中。Model
-        mv.addObject("msg","HelloSpringMVC!");
-        //封装要跳转的视图，放在ModelAndView中
-        mv.setViewName("hello"); //: /WEB-INF/jsp/hello.jsp
-        return mv;
+        return "hello";
     }
-
 }
